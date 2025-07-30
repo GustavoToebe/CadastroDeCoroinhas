a463c6a2b846a6a74e043f462b3024c2adaa78be
GustavoToebe <gustavotoebe4@gmail.com>
2025-07-30 06:54:56 -0300

    first commit

---
+++ src/main/java/dev/gustavo/cadastrodecoroinhas/CadastroDeCoroinhasApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavo.cadastrodecoroinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeCoroinhasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeCoroinhasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gustavo/cadastrodecoroinhas/CadastroDeCoroinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavo.cadastrodecoroinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeCoroinhasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
